d96835b8aef9e97dcb46a5dd779d2cdace0360b2
austencloud <68725581+austencloud@users.noreply.github.com>
2025-04-28 18:27:57 -0500

    refactor: remove disabled PWA plugin and related configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// PWA plugin temporarily disabled
-// import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		// PWA plugin temporarily disabled
-		/*
-		VitePWA({
-			registerType: 'autoUpdate',
-			includeAssets: ['favicon.png', 'social_icons/*'],
-			manifest: {
-				name: 'The Kinetic Constructor',
-				short_name: 'KineticConst',
-				description: 'Web application for kinetic construction',
-				theme_color: '#0b1d2a',
-				background_color: '#0b1d2a',
-				display: 'standalone',
-				start_url: '/',
-				icons: []
-			},
-			workbox: {
-				globPatterns: ['**/*.{js,css,html,svg,png,ico,webp,woff,woff2}'],
-				maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB limit
-				runtimeCaching: []
-			}
-		})
-		*/
-	],
+	plugins: [sveltekit()],
 	optimizeDeps: {
 		include: [
 			'clsx',
